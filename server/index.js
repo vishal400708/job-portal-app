dc9eae61e5771b9b885c880e68481371f6ec9fcc
vishal400708 <2203051050629@paruluniversity.ac.in>
2025-12-15 17:42:04 +0530

    Day 2: User registration API working

--- server/index.js
+++ server/index.js
@@ -2,19 +2,29 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(cors());
+
+// ✅ REQUIRED BODY PARSERS
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth", authRoutes);
+
+
 
 app.get("/", (req, res) => {
   res.send("Job Portal API running");
 });
 
 const PORT = process.env.PORT || 5000;
+
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
---
+++ server/routes/authRoutes.js
@@ -0,0 +1,45 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import User from "../models/User.js";
+
+const router = express.Router();
+
+// REGISTER USER
+router.post("/register", async (req, res) => {
+   console.log("REQ BODY 👉", req.body);
+  try {
+    const { name, email, password, role } = req.body;
+
+    // check user exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+export default router;
